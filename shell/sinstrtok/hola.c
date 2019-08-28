5c9ec7f13031e392b41645004572394b01abcc7d
diegovivas <dieguitovivas@hotmail.com>
2019-08-28 00:12:47 +0000

    almost

--- shell/sinstrtok/hola.c
+++ shell/sinstrtok/hola.c
@@ -358,7 +358,36 @@ void SigintHandler(int sigint)
 	signal (SIGINT, SigintHandler);
 	fflush(stdout);
 }
+//funcionmain(pid, argumentos, num, linea, environ, cont);
+void funcionmain(pid_t pid, char **argumentos, int num, char *linea, char **environ, int cont)
+{
+	int ojo = 0;
 
+	pid = fork();
+	if (pid == 0)
+	{
+		argumentos = malloc(sizeof(char *) * (num + 1));
+		if(argumentos == NULL)
+		{
+			exit(0);
+		}
+		guardarargumentos(linea, argumentos);
+		//	flag++;
+		_path(argumentos,environ, num, cont);
+		ojo = execve(argumentos[0], argumentos,environ);
+		if (ojo == -1)
+			_printf("sh: %d: %s: not found\n", cont, argumentos[0]);
+		free(linea);
+		_free(argumentos,num);
+		exit(0);
+	}
+	else
+	{
+		wait(NULL);
+	}
+
+
+}
 int main(void)
 {
 	size_t numbytes = 0;
@@ -371,7 +400,6 @@ int main(void)
 	unsigned int a = 0;
 	char *separador = " ";       
 	unsigned int num = 0;
-	unsigned int flag = 0;
 	pid_t pid = 0;
 	extern char **environ;
 	int ojo = 0;
@@ -401,12 +429,14 @@ int main(void)
 		num = numerotokens(linea,separador);
 		if(comparar(linea) == 1)
 		{
-			if (flag != 0)
-				_free(argumentos, num);
 			free(linea);
 			exit(0);
 		}
 		if (num != 0)
+		{
+		        funcionmain(pid, argumentos, num, linea, environ, cont);
+		}
+/*
 		{
 			pid = fork();
 			if (pid == 0)
@@ -431,7 +461,8 @@ int main(void)
 				wait(NULL);
 			}
 		}
-		cont++;
+*/		cont++;
+
 
 	}
 	free(linea);
